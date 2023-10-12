fdac903e89411036ec7ff914693068d81269558d
Daniel Mateos <daniel@mateos.cc>
2023-10-13 00:35:15 +1100

    crude damage

--- Enemy.gd
+++ Enemy.gd
@@ -1,6 +1,13 @@
 extends CharacterBody2D
 
 @onready var _follow :PathFollow2D = get_parent()
+@onready var ship_sprite = get_node("Ship")
+
+
+var ship_damage_1 = preload("res://assets/ship_pirate_damage_1.png")
+var ship_damage_2 = preload("res://assets/ship_pirate_damage_2.png")
+
+var health :int = 3
 
 func _ready():
 	_follow.progress_ratio = 0
@@ -8,4 +15,8 @@ func _ready():
 	tween.tween_property(_follow, 'progress_ratio', 1, 1)
 	
 func hit():
-	print("enemy ouch")
+	health -= 1
+	if health == 2:
+		ship_sprite.set_texture(ship_damage_1)
+	elif health == 1:
+		ship_sprite.set_texture(ship_damage_2)
